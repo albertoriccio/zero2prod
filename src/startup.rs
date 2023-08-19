228223b79c75dc047f397c5e814d5fff2f05a48c
alberto <ariccio@gmail.com>
2023-08-19 19:36:53 +0200

    Fixed tests

--- src/startup.rs
+++ src/startup.rs
@@ -1,16 +1,19 @@
 use actix_web::{dev::Server, web, App, HttpServer};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 //use zero2prod::run;
 use super::routes;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let connection1 = web::Data::new(db_pool);
+    let server = HttpServer::new(move || {
         App::new()
             //.route("/", web::get().to(greet))
             //.route("/{name}", web::get().to(greet))
             .route("/health_check", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
+            .app_data(connection1.clone())
     })
     .listen(listener)?
     .run();
